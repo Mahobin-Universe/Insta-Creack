44218e351e0864affaad97e7814ad68992250984
The Badboy <117584284+THE-BADBOY@users.noreply.github.com>
2023-02-12 07:15:10 -0500

    Update Crack.py

--- Crack.py
+++ Crack.py
@@ -1,7 +1,5 @@
 import os, sys
-try:os.system('xdg-open https://youtube.com/@Niki404-Cyber')
-except:pass
 try:
-    __import__("ig").crack()
+    __import__("igx").__niki____()
 except Exception as e:
     exit(str(e))
